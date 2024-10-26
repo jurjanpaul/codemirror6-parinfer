2262a16992339f48bb818c7cbcaf3a4e9e97f9a1
Jurjan-Paul Medema <jmedema@mediquest.nl>
2024-10-26 16:24:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Parinfer in CodeMirror 6 demo
+
+Combines [Parinfer](https://shaunlebron.github.io/parinfer/) with [CodeMirror 6](https://codemirror.net/) using [Scittle](https://babashka.org/scittle/), building on [Scittle's CodeMirror example](https://babashka.org/scittle/codemirror.html).
+
+Getting there, but this is still very much a work in progress!
+
+(Nowhere near a published library yet though.)
+
+## Motivation
+Ever since I started coding in Clojure (coming from years of programming in Python), I never got rid of my addiction to Parinfer. I understand, in theory, why Clojure programmers are encouraged to work with the particular flavour of Paredit that is available for their editor of choice: editing on the exact  level of the forms that make up the program. Even so, I never managed, nor tried hard enough probably, to get those keybindings into finger memory and have my editing mind 'feel' the code in front of me as different than just lines that need to be manipulated (added, deleted, split, combined, indented, deindented).
+
+So, Parinfer is a lifesaver, keeping those forms in order. (And yes: sometimes I mess up so badly, deleting or pasting half-forms, that I need to help out a little to get the parens in the right places again. That's a cost I'm willing to pay in order to enjoy my simple keystroke editing freedom.)
+
+A couple of years ago I got it in my head that I wanted to be able to explore small, Advent of Code-style algorithm challenges in Clojure on my mobile phone, just to make use of 'lost moments' to familiarise myself further with Clojure idioms for approaching typical problems. Using Paredit on an iPhone keyboard, without Ctrl and Alt keys that should somehow be combined with other keys, would not only be unattractive, but also completely impossible. So, Parinfer was the obvious and only solution to provide any kind of structural editing. Using Scittle and [Shaun le Bron's parinfer-codemirror library](https://github.com/shaunlebron/parinfer-codemirror), I was able to get the ['Away from Preferred Editor ClojureScript Playground'](https://jurjanpaul.github.io/ape-cljs-playground/) working to my satisfaction.  Note: there is nothing polished about it, but it works (and I eventually used it to solve all the 2023 Advent of Code challenges on my phone).
+
+All this time I have been well aware that CodeMirror 5, which I am using there, had already been superseded by CodeMirror 6, a complete rewrite, for which no ready-made Parinfer plugin exists. In the short term I didn't mind not being able to upgrade, but in the long run it is typically undesirable to be stuck on an old software version. Also, I was starting to worry that Parinfer support in editors was going to be a thing of the past. I had already seen the demise of the Atom editor, which had great Parinfer integration, being replaced by VS Code, which is really limited - on purpose - in how well Parinfer can be integrated. A dark scenario seemed already unfolding: because of limited editor support Parinfer's reputation was getting worse, and as a result Editor (plugin) builders would care even less about building or maintaining support for it. That's why I finally decided that I should make the integration of Parinfer into CodeMirror 6 my next side project, just to learn what it would take to get it done or to inspire others with some example code to do a better job.
+
+Honestly though: Parinfer has a simple API, so this should not be rocket science. CodeMirror 6 though seems/seemed rather complex, especially compared to the CodeMirror 5 API...
+
+## Experience so far
+...
+
+## TODO
+- [x] `smartMode` Parinfer
+- [ ] Highlight any errors (step 4 in [Adding Parinfer to an Editor](https://github.com/parinfer/parinfer.js/blob/master/doc/integrating.md))
+- [ ] Extend README
+- [ ] Maybe mark parentrail
+- [ ] Make into a published JS library
+  - [ ] Options to configure/toggle
---
+++ src/cljs/scittle-cm6-parinfer.cljs
@@ -0,0 +1,142 @@
+(require '[clojure.string :as str])
+
+;; Supports:
+;; - Parinfer smartmode
+;;
+;; Does not (yet) support:
+;; - importing as library
+;; - turning off parinfer or switching to other modes
+
+(def diff-engine
+  (js/diff_match_patch.))
+
+(defn cm-pos->parinfer-yx
+  [doc pos]
+  (let [line (.lineAt doc pos)
+        y (dec (.-number line))
+        x (- pos (.-from line))]
+    [y x]))
+
+(defn parinfer-yx->cm-pos
+  [doc y x]
+  ;; (println "line from" (.-from (.line doc (inc y))))
+  (+ (.-from (.line doc (inc y))) x))
+
+(defn cm-changeset->parinfer-changes
+  "Converts a CodeMirror 6 changeset into a Parinfer changes array."
+  [old-doc codemirror-changes]
+  (let [changes #js[]]
+    (.iterChanges
+     codemirror-changes
+     (fn [from-a to-a _from-b _to-b inserted]
+       (let [[from-a-y from-a-x] (cm-pos->parinfer-yx old-doc from-a)
+             old-text (.sliceString old-doc from-a to-a)]
+         (.push changes
+                #js{:lineNo from-a-y
+                    :x from-a-x
+                    :oldText old-text
+                    :newText (.toString inserted)}))))
+    changes))
+
+(defn- parinfer-result->cm-changes
+  [result input]
+  (let [parinferred-text (.-text result)
+        diffs (.diff_main diff-engine input parinferred-text)]
+    (.diff_cleanupSemantic diff-engine diffs)
+    (->> (js->clj diffs)
+         (reduce (fn [{:keys [pos] :as acc} diff]
+                   (let [op (aget diff 0)
+                         text (aget diff 1)]
+                     (case op
+                       0 (update acc :pos + (count text))
+                       -1 (let [new-pos (+ pos (count text))]
+                            (-> acc
+                                (update :changes conj {:from pos
+                                                       :to new-pos})
+                                (assoc :pos new-pos)))
+                       1 (update acc :changes conj {:from pos
+                                                    :to pos
+                                                    :insert text}))))
+                 {:changes []
+                  :pos 0})
+         :changes)))
+
+(defn apply-parinfer-smart-with-diff
+  [transaction]
+  (let [start-state (.-startState transaction)
+        old-cursor (-> start-state .-selection .-main .-head)
+        old-doc (.-doc start-state)
+        [old-y old-x] (cm-pos->parinfer-yx old-doc old-cursor)
+        new-state (.-state transaction) ; strongly discouraged because expensive, but can't be helped
+        new-selection (-> new-state .-selection .-main)
+        new-selection-start (.-from new-selection)
+        new-state-doc (.-doc new-state)
+        [new-selection-y _new-selection-x]
+        (cm-pos->parinfer-yx new-state-doc new-selection-start)
+        new-cursor (.-head new-selection)
+        new-doc (.-doc new-state)
+        [new-y new-x] (cm-pos->parinfer-yx new-state-doc new-cursor)
+        changes (cm-changeset->parinfer-changes old-doc
+                                                (.-changes transaction))
+        new-text (.toString new-doc)
+        result (.smartMode js/parinfer
+                           new-text
+                           #js {:prevCursorX old-x
+                                :prevCursorLine old-y
+                                :cursorX new-x
+                                :cursorLine new-y
+                                :changes changes})]
+                                ;; :selectionStartLine new-selection-y})] ; turned off as it is undermining smart (paren) mode somehow?!?
+    (if (not (.-success result))
+      {} ; TODO: pass and show error somehow
+      (let [cursorX (.-cursorX result)
+            ;; _ (println "pi x" cursorX)
+            cursorLine (.-cursorLine result)
+            ;; _ (println "pi y" cursorLine)
+            changes (parinfer-result->cm-changes result new-text)
+            ;; _ (println "changes" changes)
+            new-transaction (.update new-state
+                                     (clj->js {:changes changes
+                                               :filter false}))
+            ;; new-new-text (.toString (.-doc (.-state new-transaction)))
+            ;; _ (println "new-new-text:" (str "\n" new-new-text))
+            ;; new-doc is not exactly the final doc!?
+            new-pos (parinfer-yx->cm-pos (.-doc (.-state new-transaction)) cursorLine cursorX)  ;; Offsets in this selection should refer to the document as it is after the transaction!?!?!
+            _ (println "new-pos" new-pos)]
+        {:changes changes
+         :selection (.cursor js/cm_state.EditorSelection new-pos)
+         :sequential true}))))
+
+(defn parinfer-plugin []
+  (.of (.-transactionFilter js/cm_state.EditorState)
+       (fn [transaction]
+        ;;  (println "transactionFilter parinfer-plugin current pos" (.-from (.-main (.-selection (.-state transaction)))))
+         (if (.-docChanged transaction)
+           (let [parinfer-changes (apply-parinfer-smart-with-diff transaction)]
+             (if (seq parinfer-changes)
+               #js[transaction
+                   (clj->js parinfer-changes)]
+               transaction))
+           transaction))))
+
+(defn create-editor [doc]
+  (let [start-state
+        (.create
+         js/cm_state.EditorState
+         #js {:doc doc
+              :extensions #js [;(.of js/cm_state.EditorState.tabSize 1)
+                               (.of js/cm_language.indentUnit " ")
+                               js/codemirror.basicSetup
+                               (parinfer-plugin)
+                               (js/lang_clojure.clojure)]})]
+    (js/cm_view.EditorView. #js {:state start-state
+                                         :parent (.getElementById js/document "editor")})))
+
+(def initial-doc
+  (str/trim "
+(defn foo []
+  [:bar \"baz\"])
+"))
+
+(def cm-editor
+  (create-editor initial-doc))    
